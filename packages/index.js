e73de105c71633ab8d00b23b9f52edd58eedf5c7
jackcarey <35701383+jackcarey@users.noreply.github.com>
2022-11-20 21:30:34 +0000

    update main

--- packages/index.js
+++ packages/index.js
@@ -1,4 +1,4 @@
 export { default as FilterContentHead } from './FilterContentHead.astro';
-export { default as FilterContentControl } from './FilterContentControl.astro';
+export { default as FilterContentControl } from './FilterContentControl.tsx';
 export { default as FilterContent} from './FilterContent.astro';
 
